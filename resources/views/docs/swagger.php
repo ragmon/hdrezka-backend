b4b5ef95b942299f2767e9c6e3a1073e1479c78a
Arthur Ragimov <arthur.ragimov@gmail.com>
2021-10-08 23:02:34 +0300

    fix docs

--- resources/views/docs/swagger.php
+++ resources/views/docs/swagger.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,7 +15,7 @@
 <script src="//unpkg.com/swagger-ui-dist@3/swagger-ui-bundle.js"></script>
 <script>
     const ui = SwaggerUIBundle({
-        url: "/docs/openapi.yaml",
+        url: "/docs/openapi.yml",
         dom_id: "#swagger-ui",
         presets: [
             SwaggerUIBundle.presets.apis,
@@ -28,74 +27,6 @@
             return request;
         },
     });
-</script>
-</body>
-</html>
-
-
-
-<!-- HTML for static distribution bundle build -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Swagger UI</title>
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.20.3/swagger-ui.css" />
-    <link rel="icon" type="image/png" href="./favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="./favicon-16x16.png" sizes="16x16" />
-    <style>
-        html
-        {
-            box-sizing: border-box;
-            overflow: -moz-scrollbars-vertical;
-            overflow-y: scroll;
-        }
-
-        *,
-        *:before,
-        *:after
-        {
-            box-sizing: inherit;
-        }
-
-        body
-        {
-            margin:0;
-            background: #fafafa;
-        }
-    </style>
-</head>
-
-<body>
-<div id="swagger-ui"></div>
-
-<script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.20.3/swagger-ui-bundle.js" charset="UTF-8"> </script>
-<script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.20.3/swagger-ui-standalone-preset.js" charset="UTF-8"> </script>
-<script>
-    window.onload = function() {
-
-        // Begin Swagger UI call region
-        const ui = SwaggerUIBundle({
-            "dom_id": "#swagger-ui",
-            deepLinking: true,
-            presets: [
-                SwaggerUIBundle.presets.apis,
-                SwaggerUIStandalonePreset
-            ],
-            plugins: [
-                SwaggerUIBundle.plugins.DownloadUrl
-            ],
-            layout: "StandaloneLayout",
-            validatorUrl: "https://validator.swagger.io/validator",
-            url: "https://petstore.swagger.io/v2/swagger.json",
-        })
-
-
-        // End Swagger UI call region
-
-
-        window.ui = ui;
-    };
 </script>
 </body>
 </html>
